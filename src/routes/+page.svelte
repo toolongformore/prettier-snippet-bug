44860024e38fae83d4bfd3a5d0fc92d8888f6a97
genericFJS <ZeUnableArea222@fj-strube.de>
2024-02-10 23:44:39 +0100

    Add showcase of broken functionality

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+{#snippet test(parameter)}
+  {parameter}
+{/snippet}
+
+{@render test("a")}
+<!-- Execute `npm run format` to watch the following line get mangled -->
+{@render test((()=>"a")())}
